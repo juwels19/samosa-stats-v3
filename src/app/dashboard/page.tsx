37ea62fdce46fdb80bcaf7c43eb674bf03a75b86
Julian Curalli <julian.curalli27@gmail.com>
2025-10-09 21:32:42 -0400

    added base page metadata template

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Dashboard",
+  description: "Samosa stats dashboard",
+};
+
 export default function DashboardPage() {
   return <div>This is the dashboard page</div>;
 }
