f68b05a85d486308c653feb7cb8edbf8dd03841b
Julian Curalli <julian.curalli27@gmail.com>
2025-10-09 21:30:39 -0400

    added http action for clerk webhook

--- convex/schema.ts
+++ convex/schema.ts
@@ -15,5 +15,39 @@ export default defineSchema({
     year: v.number(),
     gameName: v.string(),
     isActive: v.boolean()
+  }),
+  events: defineTable({
+    name: v.string(),
+    displayName: v.string(),
+    season: v.id('seasons'),
+    eventCode: v.string(),
+    startDate: v.string(),
+    endDate: v.string(),
+    numberOfTeamPicks: v.int64(),
+    numberOfCategoryPicks: v.int64(),
+    isComplete: v.boolean(),
+    isOngoing: v.boolean(),
+    isSubmissionClosed: v.boolean(),
+    isCountdownActive: v.boolean(),
+  }),
+  teams: defineTable({
+    name: v.string(),
+    number: v.int64()
+  }),
+  categories: defineTable({
+    label: v.string(),
+    season: v.id("seasons"),
+    isGlobal: v.boolean()
+  }),
+  picks: defineTable({
+    event: v.id('events'),
+    user: v.id('users'),
+    userFullName: v.string(),
+    displayName: v.optional(v.string()),
+    isRandom: v.boolean(),
+    score: v.number(),
+    rank: v.int64(),
+    teams: v.array(v.id('categories')),
+    categories: v.array(v.id('categories'))
   })
 })
